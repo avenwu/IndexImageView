45cd9873af92dae091a2ef2786fa2eeb9c48ba44
Aven Wu <chaobinwu89@gmail.com>
2014-10-15 15:10:10 +0800

    add test code & clean manifest

--- app/src/main/java/com/github/avenwu/imageview/demo/app/MainActivity.java
+++ app/src/main/java/com/github/avenwu/imageview/demo/app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.github.avenwu.imageview.demo.app;
 
 import android.app.Activity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+
+import com.github.avenwu.imageview.IndexImageView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final IndexImageView imageView = (IndexImageView)findViewById(R.id.iv_test);
+		imageView.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+//				imageView.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.image2));
+				imageView.setImageDrawable(getResources().getDrawable(R.drawable.image2));
+			}
+		}, 6*1000);
 	}
 }
