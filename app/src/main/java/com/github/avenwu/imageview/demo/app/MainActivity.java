25a65a2eb14f55d4e9144e434ffc551000676c8c
Aven Wu <chaobinwu89@gmail.com>
2014-10-22 11:19:40 +0800

    add test for jni

--- app/src/main/java/com/github/avenwu/imageview/demo/app/MainActivity.java
+++ app/src/main/java/com/github/avenwu/imageview/demo/app/MainActivity.java
@@ -2,12 +2,22 @@ package com.github.avenwu.imageview.demo.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	static {
+		System.loadLibrary("hello");
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		String text = stringFromJNI();
+		Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+		Log.d("TEST", text);
 	}
 
+	public native String stringFromJNI();
 }
